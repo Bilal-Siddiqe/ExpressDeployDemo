efd2a38c21afc3730b0860e609dd25cdcf676706
Asadaziz <asadazizjs@gmail.com>
2024-03-09 21:10:10 +0500

    Structure

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// Importing required modules
 const express = require('express');
-const cors = require('cors')
-
-// Creating an Express application
+const cors = require('cors');
+const port = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Users array
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
 const user = [
     {
         name: "Hassan",
@@ -16,15 +17,8 @@ const user = [
     }
 ]
 
-
-// Define a route for GET requests
 app.get('/', (req, res) => {
-    // Send response with "Hy" message
     res.status(200).json(user);
 });
 
-// Starting the server
-const port = 5000; // You can use any port you prefer
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+
