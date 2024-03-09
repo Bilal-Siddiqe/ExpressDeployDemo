c0570385852bbbdb71fbb3e709ceb1fea3974730
Asadaziz <asadazizjs@gmail.com>
2024-03-09 21:24:21 +0500

    Vercel Added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const port = 5000;
+const PORT = 9002;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
 
 const user = [
